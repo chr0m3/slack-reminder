6160052ccb9e706f63843d3258c673e3f9efaa2a
JunGu Kang <chr0m3@users.noreply.github.com>
2022-03-09 01:27:33 +0900

    chore: add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 
     // ktlint
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+
+    // kotest
+    id("io.kotest.multiplatform") version "5.1.0"
 }
 
 group = "space.jgkang"
@@ -31,6 +34,11 @@ kotlin {
     }
     sourceSets {
         val nativeMain by getting
-        val nativeTest by getting
+        val nativeTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-framework-engine:5.1.0")
+                implementation("io.kotest:kotest-assertions-core:5.1.0")
+            }
+        }
     }
 }
