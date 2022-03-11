049e16f7fb3826b57898756b7ee041c9e219f986
JunGu Kang <chr0m3@users.noreply.github.com>
2022-03-12 01:14:30 +0900

    chore: use kotlin/jvm instead of kotlin/native

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("multiplatform") version "1.6.10"
-
-    // ktlint
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
-
-    // kotest
-    id("io.kotest.multiplatform") version "5.1.0"
+    kotlin("jvm") version "1.6.10"
+    application
 }
 
 group = "space.jgkang"
-version = "1.0-SNAPSHOT"
+version = "0.0.0"
 
 repositories {
     mavenCentral()
 }
 
-kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+dependencies {
+    testImplementation(kotlin("test"))
+}
 
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
-    }
-    sourceSets {
-        val nativeMain by getting
-        val nativeTest by getting {
-            dependencies {
-                implementation("io.kotest:kotest-framework-engine:5.1.0")
-                implementation("io.kotest:kotest-assertions-core:5.1.0")
-            }
-        }
-    }
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("MainKt")
 }
