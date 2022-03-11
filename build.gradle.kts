b12a61909803184ff024dc4b3f203d0985872493
JunGu Kang <chr0m3@users.noreply.github.com>
2022-03-12 01:38:01 +0900

    chore: add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.1.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.1.0")
 }
 
 tasks.test {
