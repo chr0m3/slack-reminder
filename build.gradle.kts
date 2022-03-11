b9b4c784ba0f8a65361ab8b3873731c156e0719f
JunGu Kang <chr0m3@users.noreply.github.com>
2022-03-12 01:20:26 +0900

    chore: add ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.10"
     application
+
+    // ktlint
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 group = "space.jgkang"
