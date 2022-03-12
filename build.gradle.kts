7d26dd57fd2a6fd38d29a3a6a743fa1b8fdf2944
JunGu Kang <chr0m3@users.noreply.github.com>
2022-03-12 22:20:50 +0900

    build: fix gradle build to make the jar file can be executed

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,3 +34,29 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
+tasks.jar {
+    // Add manifest.
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+
+    /**
+     * Add all dependencies.
+     * @see <a href="https://docs.gradle.org/current/userguide/working_with_files.html#sec:creating_uber_jar_example">
+     *         Gradle Documentation - Creating "uber" or "fat" JARs
+     *     </a>
+     */
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter {
+            it.name.endsWith("jar")
+        }.map {
+            zipTree(it)
+        }
+    })
+
+    // Remove duplicates.
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
